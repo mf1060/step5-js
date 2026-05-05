b40bacb9df73e047df0c96eda6cfe852de50c8b9
mnfurey25 <mf10604151@sju.edu>
2026-05-04 21:39:28 -0400

    adding code for a drop down menu

--- shop-frontend/src/ItemPage.jsx
+++ shop-frontend/src/ItemPage.jsx
@@ -1,13 +1,49 @@
 import Card from './Card';
 import QuantityToggle from './QuantityToggle';
-
+import { useState } from 'react';
 //This is the component for each individual item page.
 function ItemPage(props){
-    //Creates a state variable for the data related to the item
+    //Sets the url for the server
+    const API_URL = 'http://localhost:5000/items';
+    //Sets states for the quantity
+
+    const [inCartPage, setInCartPage] = useState(false);
+    const [quantity, setQuantity] = useState(0);
+    //Uses a side-by-side layout with the following reference: https://getbootstrap.com/docs/4.0/layout/grid/ 
+
+     //Function for updating the server when users add or substract items
+    const updateDropdown = (id, quantityVar) => {
+        //Set in cart page to true. 
+        setInCartPage(true);
+        setQuantity(props.item.quantity)
     
-    return (
-        <>
-         {/*Uses a side-by-side layout with the following reference: https://getbootstrap.com/docs/4.0/layout/grid/ */}
+        //Creates an empty update object that will be passed to a PATCH request to the server
+        let update = {}
+        
+        //If a user adds an item to the cart that was not previously added
+        if (quantity == 0) {
+            //Set quantity=1 and inCart = true
+            update = {
+                'quantity': quantityVar,
+                'inCart': true
+            }
+        // For all other situations, update the number of items selected in the server.
+        } else {
+            update = {'quantity': quantity + quantityVar}
+        }
+
+        //Sends the update to the server.
+        fetch(`${API_URL}/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(update),
+        });
+
+    }
+
+    const page = (
         <div class = "container">
            <div class="row">
                 <div class="col">
@@ -21,12 +57,46 @@ function ItemPage(props){
                 </div>
                 <div class="col">
                     <h1>In Stock</h1>
-                    <QuantityToggle id = {props.item.id} quantity={props.item.quantity} />
+                    {/*Using a select type, with the 
+                    following reference: https://www.w3schools.com/tags/tag_select.asp */}
+                    <select id="add-cart" name="add-cart">
+                        {[1, 2, 3, 4, 5].map((val) => {
+                            return (
+                            <option id="add-cart" value={val}>{val}</option>
+                            )
+                        })}
+                        
+                    </select>
+                    <br />
+                    {/*Only displays the "Add to Cart" button
+                    Typing to a number with the following reference:
+                    https://www.w3schools.com/js/js_datatypes.asp */}
+                    <button id="add-to-cart" data-bs-toggle="offcanvas" 
+                    data-bs-target="#offcanvasRight" 
+                    aria-controls="offcanvasRight" 
+                    onClick={() => {
+                        updateDropdown(props.item.id, 
+                    Number(document.getElementById("add-cart").value))}} class="btn btn-primary">Add to Cart</button>
                 </div>
             </div>   
-        </div>
+        </div>)
+
+    const cartPage = (
+        <>  
+            <i id="green-check" class="bi bi-check-circle"></i>
+            <a href={`/${props.item.id}`}><img id="small-img" class="card-img-top" src={props.item.img} alt={props.item.altImg} /></a>
+            <h1>It's in the cart!</h1>
         </>
     )
+    
+    
+    return (
+
+        <>
+            {inCartPage ? page: cartPage}
+        </>
+
+    );
 
 
 }
