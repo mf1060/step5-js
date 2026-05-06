0c2440acaa7082ebb81aaa3db56777fb6f0b9e4a
mnfurey25 <mf10604151@sju.edu>
2026-05-06 18:09:30 -0400

    add default values to checkout forms

--- shop-frontend/src/ContactForm.jsx
+++ shop-frontend/src/ContactForm.jsx
@@ -10,19 +10,19 @@ function ContactForm(){
         <form>
             <div class="form-row">
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="First name" /><br />
-                    <input type="text" class="form-control" placeholder="Last name" /><br />
+                    <input type="text" defaultValue="Steve" class="form-control" placeholder="First name" /><br />
+                    <input type="text" defaultValue="Rogers" class="form-control" placeholder="Last name" /><br />
                 </div>
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Email" /> <br />
-                    <input type="text" class="form-control" placeholder="Phone" /> <br />
-                    <input type="text" class="form-control" placeholder="Address, Line 1" /> <br />
+                    <input type="text" defaultValue="srogers@avengers.gov" class="form-control" placeholder="Email" /> <br />
+                    <input type="text" defaultValue="555-555-5555" class="form-control" placeholder="Phone" /> <br />
+                    <input type="text" defaultValue="1234 Marvel Ln." class="form-control" placeholder="Address, Line 1" /> <br />
                     <input type="text" class="form-control" placeholder="Address, Line 2" /> <br />
                 </div>
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="City" /> <br />
-                    <input type="text" class="form-control" placeholder="State" /> <br />
-                    <input type="text" class="form-control" placeholder="Zip" /> <br />
+                    <input type="text" defaultValue="Brooklyn" class="form-control" placeholder="City" /> <br />
+                    <input type="text" defaultValue="NY" class="form-control" placeholder="State" /> <br />
+                    <input type="text" defaultValue="19120" class="form-control" placeholder="Zip" /> <br />
                 </div>
                 </div>
             
--- shop-frontend/src/PaymentForm.jsx
+++ shop-frontend/src/PaymentForm.jsx
@@ -9,11 +9,11 @@ function PaymentForm(){
         <form>
             <div class="form-row">
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Name on Card" /> <br />
-                    <input type="text" class="form-control" placeholder="Credit Card Number" /> <br />
-                    <input type="text" class="form-control" placeholder="Security Code" /> <br />
-                    <input type="text" class="form-control" placeholder="Expiration Date" /> <br />
-                    <input type="text" class="form-control" placeholder="Zip Code" /> <br />
+                    <input type="text" defaultValue="Steve Rogers" class="form-control" placeholder="Name on Card" /> <br />
+                    <input type="text" defaultValue="1234-5678-1314-1516" class="form-control" placeholder="Credit Card Number" /> <br />
+                    <input type="text" defaultValue="123" class="form-control" placeholder="Security Code" /> <br />
+                    <input type="text" defaultValue="01/30" class="form-control" placeholder="Expiration Date" /> <br />
+                    <input type="text" defaultValue="19120" class="form-control" placeholder="Zip Code" /> <br />
                 </div>
             </div>
         </form>
