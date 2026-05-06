09b541e1887106d0fd383646d693086c6dd17cb6
mnfurey25 <mf10604151@sju.edu>
2026-05-06 19:04:54 -0400

    adding jumbotron for first slide of carousel

--- shop-frontend/src/Carousel.jsx
+++ shop-frontend/src/Carousel.jsx
@@ -4,17 +4,6 @@ function Carousel(props){
   //The following references this source for implementing a carousel
   //https://getbootstrap.com/docs/4.0/components/carousel/
 
-    const firstItem = {
-      "id": "11",
-      "title": "Lego Robot",
-      "description": "A neat lego robot for your table setting!",
-      "inCart": false,
-      "img": "/lego_robot.jpeg",
-      "altImg": "Lego Robot",
-      "price": 12.99,
-      "quantity": 0
-    }
-
 return(
     <>
     {/*Uses centering with the following reference: https://getbootstrap.com/docs/4.0/layout/grid/ */}
@@ -22,7 +11,12 @@ return(
     <div id="product-carousel" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <Card item={firstItem} class="d-block w-100"/>
+      {/* Using Bootstrap "jumbotron class with the following reference: https://getbootstrap.com/docs/4.0/components/jumbotron/ */}
+      <div class="jumbotron">
+        <div class="jumbotron">
+          <p class="lead">The one-stop shop for any home or office needs</p>
+        </div>
+      </div>
     </div>
     {//Creating a carousel item for each product.
       props.allItems.map((item) => {
--- shop-frontend/src/Home.jsx
+++ shop-frontend/src/Home.jsx
@@ -6,11 +6,11 @@ function Home(props){
 
     return(
         <>
-        <h1>Welcome to Michael's Shop!</h1>
-            {/* Using Bootstrap "lead" class for typography, reference: https://getbootstrap.com/docs/4.0/content/typography/#lead */}
-            <p class="lead">Check out our featured items below!</p>
+            <h1 class="display-4">Welcome to Michael's Shop!</h1>
+            
             {/* Adds the Carousel Component */}
             <Carousel allItems = {props.allItems}/>
+
             <Category allItems = {props.allItems} searchCategory={"Featured Items"} />
             <Category allItems = {props.allItems} searchCategory={"Home Care"} />
             <Category allItems = {props.allItems} searchCategory={"Office Supplies"} />
